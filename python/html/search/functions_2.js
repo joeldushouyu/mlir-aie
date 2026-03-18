e9212bf3a826508c5bf1392f2d5b2e2b9705718e
joeldushouyu <joeldushouyu@users.noreply.github.com>
2026-03-18 18:20:56 +0000

    deploy: a38a925ea394e5508ab9aacbd9d4150636aad5d6

---
+++ python/html/search/functions_2.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['bfloat16_5fsafe_5fallclose_0',['bfloat16_safe_allclose',['../namespaceutils_1_1hostruntime.html#adb1d092f6ded65a723011f10c557662e',1,'utils::hostruntime']]],
+  ['buffer_5fobject_1',['buffer_object',['../classutils_1_1hostruntime_1_1xrtruntime_1_1tensor_1_1XRTTensor.html#afca1e6e1446fe1edd955c5194cee8665',1,'utils::hostruntime::xrtruntime::tensor::XRTTensor']]],
+  ['buffers_2',['buffers',['../classiron_1_1worker_1_1Worker.html#ac4538a627bae0ec105071751e472898e',1,'iron::worker::Worker']]]
+];
---
+++ python/html/search/variables_8.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['logger_0',['logger',['../namespaceiron_1_1kernel.html#aaec40ed1a00e8e2418b919563bb06436',1,'iron.kernel.logger'],['../namespaceiron_1_1program.html#ac05d8183cc41cfce06b4b42ce6d1944a',1,'iron.program.logger'],['../namespaceiron_1_1runtime_1_1runtime.html#a40db9b718445d75260233359a6e1c05a',1,'iron.runtime.runtime.logger'],['../namespaceutils_1_1hostruntime_1_1hostruntime.html#a01d38e9752a418337f2152e1a470fa02',1,'utils.hostruntime.hostruntime.logger'],['../namespaceutils_1_1hostruntime_1_1xrtruntime_1_1hostruntime.html#a03bef7dfd2a8b1fe2a48fec29f503936',1,'utils.hostruntime.xrtruntime.hostruntime.logger'],['../namespaceutils_1_1compile_1_1utils.html#ac984da1eafaaf4ce0004c8713c1483c5',1,'utils.compile.utils.logger']]]
+];
